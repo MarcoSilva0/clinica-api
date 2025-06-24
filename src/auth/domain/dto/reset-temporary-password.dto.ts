247aa5066a96675287a2a5500d1802bd1d5cfe01
Marco Silva <marcoantonio02016@outlook.com>
2025-06-24 15:01:44 -0300

    feat: remove password validatation

--- src/auth/domain/dto/reset-temporary-password.dto.ts
+++ src/auth/domain/dto/reset-temporary-password.dto.ts
@@ -8,13 +8,6 @@ export class ResetTemporaryPasswordDto {
   temporaryPassword: string;
 
   @ApiProperty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-    minUppercase: 1,
-  })
   newPassword: string;
 
   @ApiProperty()
